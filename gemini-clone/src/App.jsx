709f03bdc664c3195967ffd10772cf79e04872da
shamsrehmanpro <shamsrehmanpro@gmail.com>
2024-09-18 11:21:23 +0500

    completed frontend

--- gemini-clone/src/App.jsx
+++ gemini-clone/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Sidebar from './Components/Sidebar/Sidebar.jsx'
+import Main from './Components/Main/Main.jsx'
 
 const App = () => {
   return (
     <>
       <Sidebar />
+      <Main />
+      
     </>
   )
 }
--- gemini-clone/src/Components/Main/Main.jsx
+++ gemini-clone/src/Components/Main/Main.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 import './Main.css'
+import { assets } from '../../assets/assets'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className='main'>
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <h1><span>Hello, Dev.</span></h1>
+          <h1>How can I help you today?</h1>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful place to see on an upcomming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Briefly summarize this concept: urban planning</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activities for our work retreat</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Improve readabilty of the following code</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder='Enter a prompt here' />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className="bottom-info">
+          Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy & Gemini Apps
+          </p>
+        </div>
+      </div>
+      </div>
   )
 }
 
