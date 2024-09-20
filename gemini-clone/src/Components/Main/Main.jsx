54733bc52c8a7206e840a5a5414d0c34ac26c44f
shamsrehmanpro <shamsrehmanpro@gmail.com>
2024-09-20 10:38:00 +0500

    clearing css and html for search

--- gemini-clone/src/Components/Main/Main.jsx
+++ gemini-clone/src/Components/Main/Main.jsx
@@ -35,7 +35,7 @@ const Main = () => {
 
         <div className="main-bottom">
           <div className="search-box">
-            <input type="text" placeholder='Enter a prompt here' />
+            <p contentEditable />
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
