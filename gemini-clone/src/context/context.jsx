189831afe363d64f16d3b1b8ec8e8a66dea22cb2
shamsrehmanpro <shamsrehmanpro@gmail.com>
2024-09-21 02:29:08 +0500

    adding result and animation

--- gemini-clone/src/context/context.jsx
+++ gemini-clone/src/context/context.jsx
@@ -1,32 +1,49 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import run from "../config/gemini";
 
-export const StoreContext = createContext(null) 
-
-
+export const Context = createContext(null);
 
 const StoreContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
 
-        const onSent = async (prompt) => {
-            await run(prompt)
-        }
+  const onSent = async (prompt) => {
 
-        onSent("what is react js")
-        
+    setResultData("")
+    setLoading(true)
+    setShowResult(true)
+    setRecentPrompt(input)
+    const response = await run(input);
+    const boldRegex = /\*\*(.*?)\*\*/g; // Regular expression to match text between double asterisks
+const boldText = response.replace(boldRegex, '<strong>$1</strong>');
+    setResultData(boldText)
+    setLoading(false)
+    setInput("") 
+    
+  };
 
-    const contextValue = {
-           
-            
-    }
+      const contextValue = {
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        resultData,
+        input,
+        setInput
+      };
 
-    return(
-        <StoreContext.Provider value={contextValue}>
-        {props.children}
-        </StoreContext.Provider>
-    )
+  return (
+    <Context.Provider value={contextValue}>
+      {props.children}
+    </Context.Provider>
+  );
+};
 
-}
-
-export default StoreContextProvider
-
-
+export default StoreContextProvider;
